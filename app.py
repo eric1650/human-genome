5949b819a7954e400772c48594cc2cbc798cf912
Eric Ellestad <ericellestad@Erics-MacBook-Pro.local>
2022-03-28 16:55:40 -0700

    testing heroku with reduced dataset

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 # genome = pd.read_csv(direct_url)
 
 chromosomes = ['chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chr20', 'chr21', 'chr22', 'chrX', 'chrY']
-for chromosome in chromosomes:
+for chromosome in chromosomes[0]:
     path = 'data/' + chromosome + '.csv.zip'
     if chromosome == chromosomes[0]:
         genome = pd.read_csv(path)
